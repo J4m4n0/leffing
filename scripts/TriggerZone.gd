df3ad9cdc2ed4e5485f999f66d9f6e3f9e365f50
J4m4n0 <jamano.ramdass@student.hu.nl>
2026-04-13 12:07:39 +0200

    TriggerZone werkend met Druk E indicator

---
+++ scripts/TriggerZone.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@export var zone_id: int = 0
+
+@onready var label = $Label
+
+var speler_in_zone: bool = false
+
+func _ready():
+	label.visible = false
+
+func _process(_delta):
+	if speler_in_zone and Input.is_action_just_pressed("interact"):
+		if zone_id == GameState.huidige_stap:
+			print("Zone ", zone_id, " geactiveerd!")
+
+func _on_body_entered(body):
+	if body.is_in_group("speler"):
+		speler_in_zone = true
+		if zone_id == GameState.huidige_stap:
+			label.visible = true
+
+func _on_body_exited(body):
+	if body.is_in_group("speler"):
+		speler_in_zone = false
+		label.visible = false
